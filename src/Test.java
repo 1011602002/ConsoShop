df4d419885c2b2ec6701c6debae99cc83946206d
lenovo <qq.com>
2020-12-26 17:25:21 +0800

    验证输入用户名和密码是否正确

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println("请输入你的用户名：");
         Scanner sc= new Scanner(System.in);
-        String name=sc.next();
-        System.out.println("你的用户名为："+name);
+        String username=sc.next();
+        System.out.println("你的用户名为："+username);
 
         System.out.println("请输入你的密码：");
         String Password=sc.next();
         System.out.println("你的密码是："+Password);
 
+        File file= new File("C:\\Users\\lenovo\\IdeaProjects\\ConsoShop\\src\\user.xlsx");
+        ReadExcel readExcel=new ReadExcel();
+        User user[]=readExcel.readExcel(file);
+        for (int i=0;i<user.length;i++){
+            if (username.equals(user[i].getUsername()) && Password.equals(user[i].getPassword())){
+                System.out.println("登录成功");
+                break;
+            }else {
+                System.out.println("登录失败");
+            }
+        }
     }
 }
